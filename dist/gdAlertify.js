3dfbdcda3ada2719fd523c1e59824ac02b523f78
Felipe Gaos <luisfelipegaos@gmail.com>
2024-12-05 02:17:17 -0300

    Cambio iconos

--- dist/gdAlertify.js
+++ dist/gdAlertify.js
@@ -1,4 +1,25 @@
 (function () {
+
+    const SuccessIcon = `
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" height="20" width="20"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd"></path></svg>
+    `;
+
+    const WarningIcon = `
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" height="20" width="20"><path fill-rule="evenodd" d="M9.401 3.003c1.155-2 4.043-2 5.197 0l7.355 12.748c1.154 2-.29 4.5-2.599 4.5H4.645c-2.309 0-3.752-2.5-2.598-4.5L9.4 3.003zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z" clip-rule="evenodd"></path></svg>
+    `;
+
+    const InfoIcon = `
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" height="20" width="20"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a.75.75 0 000 1.5h.253a.25.25 0 01.244.304l-.459 2.066A1.75 1.75 0 0010.747 15H11a.75.75 0 000-1.5h-.253a.25.25 0 01-.244-.304l.459-2.066A1.75 1.75 0 009.253 9H9z" clip-rule="evenodd"></path></svg>
+    `;
+
+    const ErrorIcon = `
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" height="20" width="20"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-5a.75.75 0 01.75.75v4.5a.75.75 0 01-1.5 0v-4.5A.75.75 0 0110 5zm0 10a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
+    `;
+
+    const CloseIcon = `
+        <svg width="14" height="14" viewBox="0 0 16 16" fill="currentColor"><path d="M2.96967 2.96967C3.26256 2.67678 3.73744 2.67678 4.03033 2.96967L8 6.939L11.9697 2.96967C12.2626 2.67678 12.7374 2.67678 13.0303 2.96967C13.3232 3.26256 13.3232 3.73744 13.0303 4.03033L9.061 8L13.0303 11.9697C13.2966 12.2359 13.3208 12.6526 13.1029 12.9462L13.0303 13.0303C12.7374 13.3232 12.2626 13.3232 11.9697 13.0303L8 9.061L4.03033 13.0303C3.73744 13.3232 3.26256 13.3232 2.96967 13.0303C2.67678 12.7374 2.67678 12.2626 2.96967 11.9697L6.939 8L2.96967 4.03033C2.7034 3.76406 2.6792 3.3474 2.89705 3.05379L2.96967 2.96967Z"></path></svg>
+    `;
+
     const gdAlertifyConfig = {
         backdropColor: 'rgba(0, 0, 0, 0.5)',
         modalColor: '#fff',
@@ -178,11 +199,15 @@
             document.body.appendChild(notificationContainer);
         }
 
-        notificationContainer.className = `gd-notification-container ${position}`;
+        notificationContainer.className = `gd-notification-container toaster-container ${position}`;
 
         const notificationElement = document.createElement('div');
         notificationElement.className = `gd-notification gd-notification-${type}`;
-        notificationElement.textContent = message;
+        notificationElement.innerHTML = SuccessIcon + message;
+        notificationElement.setAttribute('data-gd-toaster', '');
+        notificationElement.setAttribute('data-theme', 'light');
+        notificationElement.setAttribute('data-styled', 'true');
+        notificationElement.setAttribute('data-type', type);
 
         notificationContainer.appendChild(notificationElement);
 
@@ -202,11 +227,11 @@
     };
 
     gdAlertify.showLoader = (config = {}) => {
-        const { title, color, spinnerColor } = { 
-            ...gdAlertifyConfig.loaderDefaults, 
-            ...config 
+        const { title, color, spinnerColor } = {
+            ...gdAlertifyConfig.loaderDefaults,
+            ...config
         };
-    
+
         // Comprobar si ya existe un loader
         const existingLoader = document.querySelector('.gd-loader');
         if (existingLoader) {
@@ -214,37 +239,37 @@
             existingLoader.querySelector('span').textContent = title;
             return; // Salir de la función
         }
-    
+
         // Crear el backdrop si no existe
         const backdrop = document.createElement('div');
         backdrop.className = 'gd-modal-backdrop';
         backdrop.style.backgroundColor = gdAlertifyConfig.backdropColor;
         document.body.appendChild(backdrop);
-    
+
         // Crear el elemento del loader
         const loaderElement = document.createElement('div');
         loaderElement.className = 'gd-loader';
-    
+
         // Agregar el spinner
         const spinner = document.createElement('div');
         spinner.className = 'gd-loader-spinner';
         spinner.style.borderTopColor = spinnerColor || gdAlertifyConfig.loaderDefaults.spinnerColor;
-    
+
         loaderElement.appendChild(spinner);
-    
+
         // Agregar el texto del loader
         const titleElement = document.createElement('span');
         titleElement.textContent = title;
         titleElement.style.color = color;
         loaderElement.appendChild(titleElement);
-    
+
         document.body.appendChild(loaderElement);
     };
 
     gdAlertify.hideLoader = () => {
         const loaderElement = document.querySelector('.gd-loader');
         const backdrop = document.querySelector('.gd-modal-backdrop');
-    
+
         // Solo proceder si existe el loader
         if (loaderElement) {
             loaderElement.classList.add('fade-out');
@@ -256,7 +281,54 @@
             document.body.removeChild(backdrop);
         }
     };
-    
+
+    gdAlertify.showLoader2 = (config = {}) => {
+        const { title, color, spinnerColor } = {
+            ...gdAlertifyConfig.loaderDefaults,
+            ...config
+        };
+
+        // Comprobar si ya existe un loader
+        const existingLoader = document.querySelector('.gd-loader');
+        if (existingLoader) {
+            // Si existe, simplemente actualiza el texto del loader
+            existingLoader.querySelector('span').textContent = title;
+            return; // Salir de la función
+        }
+
+        // Crear el backdrop si no existe
+        const backdrop = document.createElement('div');
+        backdrop.className = 'gd-modal-backdrop';
+        backdrop.style.backgroundColor = gdAlertifyConfig.backdropColor;
+        document.body.appendChild(backdrop);
+
+        // Crear el contenedor del loader
+        const loaderElement = document.createElement('div');
+        loaderElement.className = 'gd-loader-2';
+
+        loaderElement.innerHTML = '<span><svg width="40" height="40" version="1.1" xmlns="http://www.w3.org/2000/svg"><circle cx="20" cy="20" r="15"></svg></span>';
+
+        document.body.appendChild(loaderElement);
+
+        // Agregar el texto del loader
+        const titleElement = document.createElement('div');
+        titleElement.textContent = title;
+        titleElement.style.color = color;
+        loaderElement.appendChild(titleElement);
+
+        document.body.appendChild(loaderElement);
+    }
+
+    gdAlertify.hideLoader2 = () => {
+        var body = document.body;
+        var loader = document.getElementById("loader");
+
+        if (loader) {
+            // Eliminar el loader y la clase de body
+            body.classList.remove("loader");
+            loader.remove();
+        }
+    }
 
     gdAlertify.alertSuccess = (message, options = {}) => {
         gdAlertify.alert(message, 'Success', { ...options, alertType: 'success' });
